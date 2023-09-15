4a6db6220ba610139d7aa333c4a44a3283c341d7
IsGris <homka228tyk@gmail.com>
2023-09-15 18:10:27 +0500

    finished benefits
    finished benefits

--- Assets/Scripts/Tile/Benefit/BenefitTile.cs
+++ Assets/Scripts/Tile/Benefit/BenefitTile.cs
@@ -1,13 +1,38 @@
-using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(menuName = "2D/Tiles/BenefitTile")]
 public class BenefitTile : Tile
 {
-	public override bool StartUp(Vector3Int location, ITilemap tilemap, GameObject go)
+	public enum BenefitType 
+	{ 
+		Attack,
+		Armor,
+		Health
+	}
+	public BenefitType Type;
+	public uint Amount;
+
+	[SerializeField] private Sprite AttackSprite;
+	[SerializeField] private Sprite ArmorSprite;
+	[SerializeField] private Sprite HealthSprite;
+
+	public void Init(BenefitType Type, uint Amount)
 	{
-		return true;
+		this.Type = Type;
+		this.Amount = Amount;
+		switch (Type)
+		{
+			case BenefitType.Attack:
+				this.sprite = AttackSprite; 
+				break;
+			case BenefitType.Armor:
+				this.sprite = ArmorSprite;
+				break;
+			case BenefitType.Health:
+				this.sprite = HealthSprite;
+				break;
+		}
 	}
 
 }
